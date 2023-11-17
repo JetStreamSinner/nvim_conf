77f69dcb7cd0204e7715f16e5c074ad30773f348
Script <bigalex934@gmail.com>
2023-11-17 14:25:48 +0300

    Add lua lsp support

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,9 +1,16 @@
 require('mason').setup()
 require('mason-lspconfig').setup {
-    ensure_installed = { 'clangd' }
+    ensure_installed = { 'clangd', 'lua_ls' }
 }
+
+require('lspconfig').lua_ls.setup{}
+
 require('lspconfig').clangd.setup({
     filetypes = { 'cpp', 'h', 'c', 'hpp' },
     autostart = true,
-    single_file_support = true
+    single_file_support = true,
+    on_attach = function(client, bufnr)
+        client.server_capabilities.signatureHelpProvider = false
+    end,
+    capabilities = capabilities
 }) 
