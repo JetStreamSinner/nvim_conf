893734da5b674062fbbd0ca46a08a7b82879ce5a
Script <bigalex934@gmail.com>
2023-11-17 11:07:03 +0300

    Change vim to lua config type

---
+++ lua/options.lua
@@ -0,0 +1,36 @@
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.mouse = 'v'
+vim.opt.mouse = 'a'
+vim.opt.backupdir = '~/.cache/vim'
+vim.opt.cc = '80'
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.showcmd = true
+vim.opt.showmode = true
+vim.opt.showmatch = true
+vim.opt.wildmenu = true
+vim.opt.filetype = 'on'
+vim.opt.filetype.plugin = 'on'
+vim.opt.filetype.indent = 'on'
+vim.opt.syntax = 'on'
