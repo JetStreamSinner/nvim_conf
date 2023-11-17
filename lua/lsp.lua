b9acdec0b9c8c850030f72a9ac73b2a6ff91c4ef
Script <bigalex934@gmail.com>
2023-11-17 14:34:41 +0300

    Add autocompletion feature

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,16 +1,18 @@
 require('mason').setup()
 require('mason-lspconfig').setup {
-    ensure_installed = { 'clangd', 'lua_ls' }
+    ensure_installed = { 'clangd', 'lua_ls' },
+    automatic_installation = true
 }
 
-require('lspconfig').lua_ls.setup{}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+require('lspconfig').lua_ls.setup{
+    capabilities = capabilities
+}
 require('lspconfig').clangd.setup({
     filetypes = { 'cpp', 'h', 'c', 'hpp' },
     autostart = true,
     single_file_support = true,
-    on_attach = function(client, bufnr)
-        client.server_capabilities.signatureHelpProvider = false
-    end,
     capabilities = capabilities
-}) 
+})
+
